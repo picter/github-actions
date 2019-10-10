c6d1d9dd28fba030ee90309e713f785ba07f16b9
Matthias Lohscheidt <mail@mlohscheidt.de>
2019-10-11 00:30:48 +0200

    initial commit

---
+++ review-urls/src/main.ts
@@ -0,0 +1,23 @@
+import * as core from '@actions/core';
+import github from '@actions/github';
+
+async function run() {
+  try {
+    const title = core.getInput('title');
+    const urlPattern = core.getInput('urlPattern');
+    const githubToken = core.getInput('githubToken');
+
+    const octokit = new github.GitHub(githubToken);
+
+    const context = github.context;
+
+    await octokit.issues.createComment({
+      ...context.issue,
+      body: `*${title}:* ${urlPattern}`,
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
